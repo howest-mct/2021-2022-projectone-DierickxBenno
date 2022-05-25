5e6ae1b2152b96844ede8eb15c84b7f1fa7ba425
Benno Dierickx <91146451+DierickxBenno@users.noreply.github.com>
2022-05-25 13:37:03 +0200

    commit: temperatuur niet langer invasief, stappenteller te gevoelig?

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -6,8 +6,10 @@
 #include <DallasTemperature.h>
 #include <BluetoothSerial.h>
 
-float temperatureC;
-byte LED = 2;
+// timed events
+int eventtimeTemp = 5000;
+int pasteventTemp = 0;
+//
 const byte owTemp = 4;
 float hoek;
 bool tussenStap = 0;
@@ -19,65 +21,61 @@ DallasTemperature sensors(&oneWire);
 
 Adafruit_MPU6050 mpu;
 
-// const char* ssid = "ssid";
-// const char* pswd = "pswd";
-
-float testList[3] = {0, 0, 0};
+// voor OTA
 
 BluetoothSerial SerialBT;
 
-// #region timed events
-int eventtimeTemp = 1000;
-long pasteventTemp = 0;
-
 void setup()
 {
-  SerialBT.begin("DogBit-BD-1MCT1");
+  /*
+   * Login page
+   */
+
+  // basic setup
+  byte LED = 2;
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
 
-  // #region MPU setup
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  // wifi setup
+  // WiFi.begin(ssid, password);
+  // BT setup
+  SerialBT.begin("DogBit-BD-1MCT1");
 
-  Serial.println("Adafruit MPU6050 test!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  //# region MPU setup
+  while (!Serial)
+  {
+    delay(10);
+  } // will pause Zero, Leonardo, etc until serial console opens
+
+  // mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
-  // Try to initialize!
-  if (!mpu.begin())
-  {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1)
-    {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
   // # endregion MPU setup
 
   sensors.begin();
-  Serial.println("Service started");
-
-  //  WiFi.begin(ssid, pswd);
 }
 
 void loop()
 {
-  digitalWrite(LED, 1);
+  // show loop is running
+  digitalWrite(2, 1);
+  delay(10);
+  digitalWrite(2, 0);
+  //
   if ((millis() - pasteventTemp) > eventtimeTemp)
   {
+
     sendTemperature();
     pasteventTemp = millis();
   }
-  digitalWrite(LED, 0);
+
   detectSteps();
-  delay(10);
 }
 
 void sendTemperature()
 {
+  float temperatureC;
   sensors.requestTemperatures();
   temperatureC = sensors.getTempCByIndex(0);
   SerialBT.println("temperatuur: " + String(temperatureC));
@@ -91,12 +89,12 @@ void detectSteps()
 
   hoek = g.gyro.y;
   // code for step counter
-  if (hoek < 0.15)
+  if (hoek < 0.4)
   {
     tussenStap2 = 1;
   }
 
-  if (hoek > 0.15 && tussenStap2 == 1)
+  if (hoek > 0.4 && tussenStap2 == 1)
   {
     tussenStap2 = 2;
     tussenStap = !tussenStap;
